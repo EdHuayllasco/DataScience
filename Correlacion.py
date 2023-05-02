1bc431a5ec7eab3cd215aba34b4cdd33df0be06f
Edward Huayllasco <edwardhc1997@gmail.com>
2023-05-02 10:42:52 -0500

    Dataset1

--- Correlacion.py
+++ Correlacion.py
@@ -1,2 +1,20 @@
-# Free
-print(5)
+import csv
+
+users = {}
+
+with open('Movie_Ratings.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for i, row in enumerate(reader):
+        if i == 0:
+            headers = row[1:]
+        else:
+            movie = row[0]
+            for j, rating in enumerate(row[1:]):
+                if rating != "":
+                    user = headers[j]
+                    if user not in users:
+                        users[user] = {}
+                    users[user][movie] = float(rating)
+
+
+print(users)
