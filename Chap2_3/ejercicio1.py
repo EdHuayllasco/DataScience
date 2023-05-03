2bcd8cfb883c232216c471930578f7a402af0d7f
Edward Huayllasco <edwardhc1997@gmail.com>
2023-05-03 10:49:44 -0500

    Correlacion ejercicio 1 y 3 terminados

--- Chap2_3/ejercicio1.py
+++ Chap2_3/ejercicio1.py
@@ -1,12 +1,9 @@
 import csv
+from math import sqrt
 users = {}
 # Abrir el archivo CSV
 with open('./Dataset1/BX-Book-Ratings.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
-    
-    # Crear un diccionario vacío para almacenar los datos
-    
-    
     # Iterar a través de los datos
     for row in reader:
         # Obtener los valores de la fila
@@ -21,6 +18,32 @@ with open('./Dataset1/BX-Book-Ratings.csv', newline='', encoding='utf-8') as csv
         # Agregar el libro y la puntuación al diccionario del usuario
         users[user_id][book_id] = rating
 
+with open('./Dataset1/BX-Users.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    
+    # Crear un diccionario vacío para almacenar los datos
+    user_data = {}
+    
+    # Iterar a través de los datos
+    for row in reader:
+        # Obtener los valores de la fila
+        user_id = row[0]
+        location = row[1]
+        age = row[2] if row[2] != '\\N' else None
+        
+        # Dividir la ubicación en acrónimo, ciudad y país
+        location_parts = location.split(', ')
+        acronym = location_parts[0]
+        city = location_parts[1] if len(location_parts) > 1 else None
+        country = location_parts[-1] if len(location_parts) > 1 else None
+        
+        # Agregar los datos del usuario al diccionario
+        user_data[user_id] = {
+            'acronym': acronym,
+            'age': age,
+            'city': city,
+            'country': country
+        }
 
 class Similitud:
     def __init__(self, nombre1, nombre2, metrica):
@@ -32,47 +55,38 @@ class Similitud:
         self.rating2 = users[self.nombre2]
     def manhattan(self): 
         distance = 0 
-        commonRatings = False
-
-        for key in self.rating1: 
-            if key in self.rating2: 
-                distance += abs(self.rating1[key] - self.rating2[key]) 
-                commonRatings = True
-        if commonRatings: 
-                return distance 
-        else: 
-                return -1
+        common_items = set(self.rating1.keys()) & set(self.rating2.keys())
+        if len(common_items) == 0:
+            return 0
+        for key in common_items: 
+            distance += abs(self.rating1[key] - self.rating2[key]) 
+        return distance
+        
     def euclidiana(self): 
         distance = 0 
-        commonRatings = False
-
-        for key in self.rating1: 
-            if key in self.rating2: 
-                distance += pow(abs(self.rating1[key] - self.rating2[key]),2) 
-                commonRatings = True
-        if commonRatings: 
-                return pow(distance, 1/2)
-        else: 
-                return -1
+        common_items = set(self.rating1.keys()) & set(self.rating2.keys())
+        if len(common_items) == 0:
+            return 0
+        for key in common_items: 
+            distance += pow(abs(self.rating1[key] - self.rating2[key]),2) 
+        return pow(distance, 1/2)
     def Pearson(self): 
         multi = 0 
         sumrating1 = 0
         sumrating2 = 0
         sqrating1 = 0
         sqrating2 = 0
-        aux = 0
         distance = 0
-
-        for key in self.rating1: 
-            if key in self.rating2: 
-                multi += self.rating1[key] * self.rating2[key]
-                sumrating1 += self.rating1[key]
-                sqrating1 += self.rating1[key]**2
-                sumrating2 += self.rating2[key]
-                sqrating2 += self.rating2[key]**2
-                aux += 1
-        if aux == 0: 
+        common_items = set(self.rating1.keys()) & set(self.rating2.keys())
+        aux = len(common_items)
+        if aux == 0:
             return 0
+        for key in common_items: 
+            multi += self.rating1[key] * self.rating2[key]
+            sumrating1 += self.rating1[key]
+            sqrating1 += self.rating1[key]**2
+            sumrating2 += self.rating2[key]
+            sqrating2 += self.rating2[key]**2
         deno = sqrt(sqrating1 - (sumrating1**2)/aux)*sqrt(sqrating2 - (sumrating2**2)/aux)
         if deno == 0:
             return 0
@@ -84,17 +98,10 @@ class Similitud:
         print(common_items)
         if len(common_items) == 0:
             return 0
-        multi = 0 
-        sqrating1 = 0
-        sqrating2 = 0
-
-        for key in self.rating1: 
-            sqrating1 += self.rating1[key]**2
-            if key in self.rating2: 
-                multi += self.rating1[key] * self.rating2[key]
-        for key in self.rating2:
-            sqrating2 += self.rating2[key]**2
-        distance = multi / (sqrt(sqrating1) *  sqrt(sqrating2))
+        numerador = sum(self.rating1[item] * self.rating2[item] for item in common_items)
+        denominador1 = sum(self.rating1[item]**2 for item in self.rating1.keys())
+        denominador2 = sum(self.rating2[item]**2 for item in self.rating2.keys())
+        distance = numerador / (sqrt(denominador1) *  sqrt(denominador2))
         return distance
     def calcular_similitud(self):
         if self.metrica == 'manhattan':
@@ -106,10 +113,7 @@ class Similitud:
         elif self.metrica == 'pearson':
             similitud = self.Pearson()
         print(similitud)
-# print(manhattan(users['Heather'], users['Bryan']))
-# print(euclidiana(users['Heather'], users['Bryan']))
-# print(Pearson(users['Heather'], users['Bryan']))
-# print(Cosine(users['Heather'], users['Bryan']))
+
 name1 = input("Ingrese el nombre de la primera persona: ")
 name2 = input("Ingrese el nombre de la segunda persona: ")
 metric_type = input("Ingrese el tipo de métrica (manhattan, euclidiana, cosine, pearson): ")
@@ -117,3 +121,6 @@ sim = Similitud(name1, name2, metric_type)
 sim.calcular_similitud()
 # Imprimir el diccionario para el usuario con ID "276725" como ejemplo
 # print(users.get('276747', {}))
+
+# Imprimir el diccionario para el usuario con id "1" como ejemplo
+# print(user_data.get('5555', {}))
